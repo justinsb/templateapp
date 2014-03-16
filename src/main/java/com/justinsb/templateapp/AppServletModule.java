97709533357541363c5d42797ed0fe0136ac7989
Justin SB <justin@fathomdb.com>
2014-03-16 19:51:05 -0400

    Register the ping servlet

--- src/main/java/com/justinsb/templateapp/AppServletModule.java
+++ src/main/java/com/justinsb/templateapp/AppServletModule.java
@@ -3,12 +3,14 @@ package com.justinsb.templateapp;
 import com.google.inject.servlet.ServletModule;
 import com.justinsb.templateapp.api.CORSFilter;
 import com.justinsb.templateapp.api.ExampleEndpoint;
+import com.justinsb.templateapp.api.PingEndpoint;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
 
 class AppServletModule extends ServletModule {
     @Override
     protected void configureServlets() {
         bind(ExampleEndpoint.class);
+        bind(PingEndpoint.class);
 
         filter("/api/*").through(CORSFilter.class);
 
